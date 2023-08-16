f85213d2c99bcfefbbb1a48166a9cff04e7cfac0
Nestadar <Nestadar@gmail.com>
2023-08-16 17:21:35 +0200

    coma added kek

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
